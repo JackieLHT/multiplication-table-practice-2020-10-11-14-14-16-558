27f3a5091f6fe9685851eec1d9cd1a9afc3fe4e7
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-24 23:32:20 +0800

    added PDCA

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,6 +2,11 @@ package com.tw;
 
 public class MultiplicationTable {
     String multiplicationTable = "";
+    
+//P:10 min
+//D:22 min
+//C: Much time was used to check the formatting of the output string
+//A: Can clarify more in the analyzing question step before doing the actual coding
     public String createMultiplicationTable(int startNumber, int endNumber) {
         if(!isStartSmallerThanOrEqualEnd(startNumber,endNumber))
             return null;
@@ -12,14 +17,26 @@ public class MultiplicationTable {
         
     }
 
+//P:1 min
+//D:1 min
+//C: The time taken is as expected.
+//A: 
     public boolean isStartSmallerThanOrEqualEnd(int startNumber, int endNumber) {
         return startNumber <= endNumber;
     }
 
+//P:1 min
+//D:1 min
+//C: The time taken is as expected.
+//A: 
     public boolean isInRangeOf1000(int startNumber, int endNumber) {
         return (startNumber >= 1) && endNumber <= 1000;
     }
 
+//P: 5 min
+//D: 10 min
+//C: Extra time was used as the requirement of the output string was not fully understood at first and I spent some time to figure out the correct way of formatting the string
+//A: Make sure the requirement is fulling understood and ask for clarification if not clear
     public String generateMultiplicationTable(int startNumber, int endNumber)
     {
         for(int i = startNumber; i <= endNumber; i++) {
@@ -30,6 +47,10 @@ public class MultiplicationTable {
         return multiplicationTable;
     }
 
+//P: 5 min
+//D: 10 min
+//C: Extra time was used as the requirement of the output string was not fully understood at first and I spent some time to figure out the correct way of formatting the string
+//A: Make sure the requirement is fulling understood and ask for clarification if not clear
     public String generateMultiplicationLine(int startNumber, int intermediate)
     {
         String line = "";
